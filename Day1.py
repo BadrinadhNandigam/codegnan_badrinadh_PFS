46e1999afbf129b2868432a32eb93a69c38fed9a
BadrinadhNandigam <nandigambadrinadh4@gmail.com>
2026-05-13 15:27:16 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,48 @@
+'''
+print("Hello world")
+
+
+num = 22
+st = "python"
+li = [1,2]
+dic = {"Name" : "Badri"}
+
+print(num)
+print(st)
+print(li)
+print(dic)
+'''
+
+'''
+variables
+---------------
+good ways
+------
+A-z,a-z,_
+
+bad ways
+---------------
+special char,space, 0-9
+'''
+
+'''
+a = 90
+A= 9
+a_i = 09
+4b = 09
+'''
+
+'''
+Comments
+--------------
+1. single line (#)
+'''
+
+Name = "Badrinadh"   #String
+Age = 22             #int
+Salary = 26399.01   #float
+
+print(Name)
+print(Age)
+print(Salary)
+
